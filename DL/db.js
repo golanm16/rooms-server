ba31d6f29602d61142b189d75feb627164c69d5c
Golan <golanm16@gmail.com>
2022-02-20 09:25:09 +0200

    started testing

--- DL/db.js
+++ DL/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 exports.connect = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URL,
+      process.env.MONGO_PRIVATE,
       {
         useNewUrlParser: true,
       },
